e4d56e0d96f7afa2d73584eccde45aaf3d214f83
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-09-28 18:20:13 -0400

    Ran 'rake db:migrate'.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20100928221642) do
+
+  create_table "pictures", :force => true do |t|
+    t.string   "filename"
+    t.string   "title"
+    t.string   "description"
+    t.string   "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
