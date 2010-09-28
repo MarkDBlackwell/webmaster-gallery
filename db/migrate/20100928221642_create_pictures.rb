a956fc75b21e20a7e88379a9126e297fa6f96767
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-09-28 18:17:53 -0400

    Ran 'rails generate model Picture filename:string title:string description:string year:string'.

---
+++ db/migrate/20100928221642_create_pictures.rb
@@ -0,0 +1,16 @@
+class CreatePictures < ActiveRecord::Migration
+  def self.up
+    create_table :pictures do |t|
+      t.string :filename
+      t.string :title
+      t.string :description
+      t.string :year
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :pictures
+  end
+end
