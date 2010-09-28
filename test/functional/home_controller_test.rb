46ee0df7aa7f187c3079b13498a634c960a1ab17
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-09-28 16:42:39 -0400

    Changed for home redirect to /pictures'.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should redirect to '/pictures'" do
     get :index
-    assert_response :success
+    assert_redirected_to :controller => "pictures", :action => "index"
   end
 
 end
