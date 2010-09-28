3ccdd22982b076949bb0a1194ee943fcb0f8bc51
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-09-28 15:41:36 -0400

    Ran 'rails new'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = '1bbd96654743bd7bd2b5b5698de60cafb62bb810006c2e08f6fff06407e0a8d08c61124b8cfcd5aa075b20306a759752ea9469630880856092cb8f378d92b64d'
