ca178b49c4915a9da2acb35b07b7fa88f0069441
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-10-07 17:24:21 -0400

    Created sha2 digest program.

---
+++ digest-sha2.rb
@@ -0,0 +1,21 @@
+# Author: Mark D. Blackwell
+# Date written: October 7, 2010.
+# Date last updated: October 7, 2010.
+
+# Reference 'encrypting data with ruby and OpenSSL' at: 
+# http://snippets.dzone.com/posts/show/576
+
+require 'English'
+require 'fileutils'
+require 'digest/sha2'
+
+clear_text_string='apple'
+name=%w[/home mark rails-apps gallery test fixtures files 
+file_password 
+  encrypted-password.txt].join('/')
+f=File.new(name,'w')
+s=Digest::SHA2.digest(clear_text_string)
+puts '$\ not nil' unless $OUTPUT_RECORD_SEPARATOR.nil?
+f.print(s)
+f.close
+puts "#{File.size(name)} bytes written to #{name}."
