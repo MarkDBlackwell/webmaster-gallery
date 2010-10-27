3f6ea607f90dfd4db8ada4af5fc97128ef05b721
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-10-27 14:05:58 -0400

    Copied pictures helper test preparatory to splitting.

--- test/unit/helpers/sessions_helper_test.rb
+++ test/unit/helpers/sessions_helper_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SessionsHelperTest < ActionView::TestCase
 
+  test "should include this file" do
+#    flunk
+  end
+
   test "should be no Sessions helpers, or write tests" do
     f=File.open("#{Rails.root}/app/helpers/sessions_helper.rb",'r')
     assert_equal ["module SessionsHelper\n","end\n"], f.readlines
