e9cb0c4fe0ca9bbb3f11036458a22605282ca487
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-11-03 21:13:34 -0400

    AdminPictures templates show and edit unified.

--- app/controllers/admin_pictures_controller.rb
+++ app/controllers/admin_pictures_controller.rb
@@ -4,7 +4,7 @@ class AdminPicturesController < ApplicationController
   def index
     return unless check_request
     return unless check_logged_in_and_redirect
-    @editable = true
+    @editable=true
     @tags = Tag.all
     @pictures = Picture.all
   end
@@ -12,13 +12,17 @@ class AdminPicturesController < ApplicationController
   def show
     return unless check_request
     return unless check_logged_in_and_redirect
+    @show_filename=true
+    @editable=true
     @picture = Picture.find(params[:id])
-    @editable = true
+    render :action => :edit # Show the record.
   end
 
   def edit
     return unless check_request
     return unless check_logged_in_and_redirect
+    @show_filename=true
+    @edit_fields=true
     @picture = Picture.find(params[:id])
   end
 
@@ -30,8 +34,15 @@ class AdminPicturesController < ApplicationController
     [:description,:title,:year].each do |e|
       @picture[e]=pp.fetch e if pp.has_key? e
     end unless pp.blank?
-    @editable = true
-    render :action => @picture.save ? :show : :edit
+    @show_filename=true
+    if @picture.save
+      @editable=true
+#      render :action => :show
+      render :action => :edit # Show the record.
+    else
+      @edit_fields=true
+      render :action => :edit
+    end
   end
 
 end
