e1a58b0935eb2de2eb0152e7a38f1e490675003b
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-11-15 17:12:45 -0500

    Add all layouts test.

--- test/unit/layouts/all_layout_test.rb
+++ test/unit/layouts/all_layout_test.rb
@@ -9,7 +9,7 @@ class AllLayoutTest < ActionView::TestCase
   end
 
   test "should have the right document type" do
-    assert_equal '<!DOCTYPE html>', (rendered.slice 0...rendered.index("\n"))
+    assert_doctype
   end
 
 #-------------
@@ -23,18 +23,19 @@ class AllLayoutTest < ActionView::TestCase
     assert_select 'html head', 1
   end
 
-  test "head tag should include one title tag" do
+  test "head tag should first include one title tag" do
     assert_select 'head title', 1
     assert_select 'head > title:first-child', 1
   end
 
   test "head tag should include six script tags" do
     assert_select 'head script', 6
+    assert_select 'head script[type=text/javascript]', 6
   end
 
-  test "head tag should include certain script tags" do
-    %w[prototype effects dragdrop controls rails application
-        ].each_with_index do |e,i|
+  test "head tag should include certain script tags in order" do
+    %w[prototype effects dragdrop controls rails application].
+        each_with_index do |e,i|
       assert_select "head title + #{'* + '*i} script[src=?]",
           Regexp.new(%Q@/javascripts/#{e}\\.js\\?\\d*\\z@)
     end
@@ -42,8 +43,8 @@ class AllLayoutTest < ActionView::TestCase
 
   test "head tag should include one style tag" do
     assert_select 'head style', 1
-    assert_select 'head style.styles', 1
     assert_select 'head > style:last-child', 1
+    assert_select 'head style.styles[type=text/css]', 1
   end
 
   test "html tag should include one body tag" do
@@ -53,9 +54,31 @@ class AllLayoutTest < ActionView::TestCase
 #-------------
   private
 
-  def setup(*args)
-    setup_with_controller unless args.empty?
-    render_layout "#{Rails.root}/app/views/layouts/application", *args
+  def assert_doctype
+    s="<!DOCTYPE html>\n"
+    render_all_layouts {assert (rendered.start_with? s), s}
+  end
+
+  def assert_select(*args)
+    render_all_layouts {super}
+  end
+
+  def render_all_layouts
+    @filenames.each do |f|
+      setup_with_controller if @need_reload
+      @need_reload=true
+      render_layout f
+      yield
+    end
+  end
+
+  def setup
+    @need_reload=false
+    unless @filenames
+      d="#{Rails.root}/app/views/layouts"
+      @filenames=((Dir.entries d) - %w[.. .]).
+          collect {|e| "#{d}/#{e.chomp '.html.erb'}" }
+    end
   end
 
 end
