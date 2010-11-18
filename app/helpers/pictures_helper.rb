457d8f7ed70dedc54244bb333c865beea22aa125
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-11-17 22:30:40 -0500

    Move pictures field_helper to partial, 'field'.

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,22 +1,7 @@
 module PicturesHelper
-# Helpers should return nil, always.
 
   def alltags
     concat( render :partial => 'pictures/all_tags' )
-    nil
-  end
-
-  def field_helper(record,field)
-    value=record[field]
-    label=raw(' &nbsp; ').concat field
-    uneditable=[:filename]
-    plain=(uneditable.include? field) || ! @edit_fields
-    hide=:filename==field && ! @show_filename
-
-    return if hide
-    concat( plain ? value : text_field_tag(record, field,
-        :value => value, :name => "picture[#{field}]") )
-    concat( content_tag :div, label, :class => 'label' ) if @show_labels
     nil
   end
 
