d28981dace05be36cc0b313b8c8e9d75351a2540
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-11-20 23:01:01 -0500

    Refactor to work pictures partial, 'picture' test.

--- test/unit/partials/pictures/picture_test.rb
+++ test/unit/partials/pictures/picture_test.rb
@@ -2,63 +2,56 @@ require 'test_helper'
 
 class PicturePicturesPartialTest < SharedPartialTest
 
-# 13 tests, 33 assertions
-
   test "should render" do
     assert_template :partial => 'pictures/_picture'
   end
 
-  PREFIX1='div.picture'
   test "should include one picture div" do
-    assert_select "#{PREFIX1}", 1
+    assert_select 'div.picture', 1
   end
 
   test "should render a single, right picture" do
-    assert_select "#{PREFIX1}[id]", 1
-    assert_select "#{PREFIX1}[id=picture_#{@picture.id}]", 1
+    assert_select 'div.picture[id]', 1
+    assert_select "div.picture[id=picture_#{@picture.id}]", 1
   end
 
-  PREFIX2='div.picture > form > div.'
   test "should render a single thumbnail within a picture" do
-    assert_select "#{PREFIX2}thumbnail", 1
+    assert_select 'div.picture > form > div.thumbnail', 1
   end
 
-  PREFIX3='div.picture > form > div.field > div.'
   test "should render a single, right year within a picture" do
-    has_one "#{PREFIX3}year", '2002'
+    has_one 'div.picture > form > div.field > div.year', '2002'
   end
 
   %w[description sequence title weight].each do |unique|
     test "should render a single, right #{unique} within a picture" do
-      has_one "#{PREFIX3}#{unique}", "two-#{unique}"
+      has_one "div.picture > form > div.field > div.#{unique}", "two-#{unique}"
     end
   end
 
   test "should render a single, right filename within a picture if show "\
        "filename" do
-    has_one("#{PREFIX3}filename",
+    has_one('div.picture > form > div.field > div.filename',
         'two.png') {@show_filename = true}
   end
 
-  PREFIX4='div.picture > div.edit'
   test "should render a single edit div within a picture if editable" do
-    assert_select "#{PREFIX4}", false
+    assert_select 'div.picture > div.edit', false
     setup {@editable = true}
-    assert_select "#{PREFIX4}", 1
+    assert_select 'div.picture > div.edit', 1
   end
 
-  PREFIX5='div.picture > div.edit > form.button_to'
   test "should render a single button within an edit div if editable" do
-    assert_select "#{PREFIX5}", false
+    assert_select 'div.picture > div.edit > form.button_to', false
     setup {@editable = true}
-    assert_select "#{PREFIX5}", 1
+    assert_select 'div.picture > div.edit > form.button_to', 1
   end
 
   test "rendered button within an edit div should have method get" do
-    assert_select "#{PREFIX5}[method]", false
+    assert_select 'div.picture > div.edit > form.button_to[method]', false
     setup {@editable = true}
-    assert_select "#{PREFIX5}[method]", 1
-    assert_select "#{PREFIX5}[method=?]", 'get'
+    assert_select 'div.picture > div.edit > form.button_to[method]', 1
+    assert_select 'div.picture > div.edit > form.button_to[method=?]', 'get'
   end
 
 #-------------
