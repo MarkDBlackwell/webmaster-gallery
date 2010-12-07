58d94af0c81e9b91199884a425c6c7c86574e692
Mark D. Blackwell <markdblackwell01@gmail.com>
2010-12-07 18:15:30 -0500

    Move test methods to editupdate shared.

--- test/functional/sessions/_shared.rb
+++ test/functional/sessions/_shared.rb
@@ -3,59 +3,12 @@ class SharedSessionsControllerTest < SharedControllerTest
 
   private
 
-  def construct_added_pictures
-    added=['three.png']
-    expected=Picture.find(:all).map(&:filename).take(1).concat added
-    [expected,added]
-  end
-
-  def construct_added_tags
-    added=['three-name']
-    expected=Tag.find(:all).map(&:name).take(1).concat added
-    [expected,added]
-  end
-
-  def construct_deleted_pictures
-    expected=Picture.find(:all).map &:filename
-    deleted=expected.pop 1
-    [expected,deleted]
-  end
-
-  def construct_deleted_tags
-    expected=Tag.find(:all).map &:name
-    deleted=expected.pop 1
-    [expected,deleted]
-  end
-
-  def construct_unpaired
-    collection= %w[a b]
-  end
+# Working_on
 
   def login(p=nil)
     p=get_password if p.blank?
     set_cookies
     post :create, :password => p
-  end
-
-# Working_on
-
-  def mock_directory_pictures(expected)
-    expected=Picture.find(:all).map       &:filename if :all==expected
-    mock_model expected, DirectoryPicture, :filename
-  end
-
-  def mock_unpaired(expected)
-    DirectoryPicture.expects(:find_unpaired).returns expected
-  end
-
-  def mock_file_tags(expected)
-    expected=Tag.find(:all).map  &:name if :all==expected
-    mock_model expected, FileTag, :name
-  end
-
-  def mock_model(expected,model,method)
-    model.expects(:find).returns(expected.
-        collect {|e| (p=model.new).expects(method).returns e; p} )
   end
 
   def pictures_in_layouts_directory?
--- test/functional/sessions/editupdate/_shared.rb
+++ test/functional/sessions/editupdate/_shared.rb
@@ -4,4 +4,51 @@ class SharedEditUpdateSessionsControllerTest < SharedSessionsControllerTest
 
   private
 
+  def construct_added_pictures
+    added=['three.png']
+    expected=Picture.find(:all).map(&:filename).take(1).concat added
+    [expected,added]
+  end
+
+  def construct_added_tags
+    added=['three-name']
+    expected=Tag.find(:all).map(&:name).take(1).concat added
+    [expected,added]
+  end
+
+  def construct_deleted_pictures
+    expected=Picture.find(:all).map &:filename
+    deleted=expected.pop 1
+    [expected,deleted]
+  end
+
+  def construct_deleted_tags
+    expected=Tag.find(:all).map &:name
+    deleted=expected.pop 1
+    [expected,deleted]
+  end
+
+  def construct_unpaired
+    collection= %w[a b]
+  end
+
+  def mock_directory_pictures(expected)
+    expected=Picture.find(:all).map       &:filename if :all==expected
+    mock_model expected, DirectoryPicture, :filename
+  end
+
+  def mock_file_tags(expected)
+    expected=Tag.find(:all).map  &:name if :all==expected
+    mock_model expected, FileTag, :name
+  end
+
+  def mock_model(expected,model,method)
+    model.expects(:find).returns(expected.
+        collect {|e| (p=model.new).expects(method).returns e; p} )
+  end
+
+  def mock_unpaired(expected)
+    DirectoryPicture.expects(:find_unpaired).returns expected
+  end
+
 end
