bf5a8a2b0d66229c4c672d2a901e65de0d078f9a
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-01-05 17:19:43 -0500

    Test various generated extensions.

--- app/models/square.rb
+++ app/models/square.rb
@@ -5,6 +5,12 @@ class Tesseract; def self.new(*a) Hypercube.new 4, *a end end
 class Power; def self.new(power,*a) Hypercube.new power, *a end end
 
 class Hypercube
+# Gives, e.g. every triplet from a triplicated vector.
+# Usage examples:
+#   Hypercube.new(3, (0..7).to_a).map{|i,j,k| block}
+#   Square.new.map{|i,j| block}
+#   Cube.new.map{|i,j,k| block}
+
   def self.new(power,array=[0,1])
     p,a=power.to_i,array.to_a
     (2..p).inject(a){|m,trash| m.product a}.flatten.each_slice p
--- test/unit/models/directory_picture/_test.rb
+++ test/unit/models/directory_picture/_test.rb
@@ -16,35 +16,62 @@ class DirectoryPictureTest < ActiveSupport::TestCase
     assert_equal 6, DirectoryPicture.find_unpaired_names.length
   end
 
+  test "names with various extensions..." do
+    thumbnail_indicator=[?-,?t]
+    replace=?a
+    dot=?.
+    good_c = [?A..?Z, ?a..?z, ?0..?9, [?-, ?.] ].map{|e| e.to_a}.flatten
+    good=Square.new((0..4).to_a).map do |a|
+      name,x=a.map{|i| Array.new(i).map{good_c.choice}}
+      x.map!{|e| e!=dot ? e : replace}
+      (name.pop; name.push replace) if name.last==dot
+      name[-2*t.length,t.length]=t if (t=thumbnail_indicator)*2==(name.last 4)
+      (x.present? ? [name+[dot]+x] : [name+[dot], name]).map{|e| e.to_s}
+    end.flatten
+    assert_present good
+# Should all be good:
+# Finding good & bad names should find all & none:
+    [   [good,  good.length, 0         ] ].each do
+        |names, good_length, bad_length|
+      called=2
+      dp=DirectoryPicture
+      dp.expects(:gallery_directory        ).times(called).returns @tests
+      dp.expects(:gallery_directory_entries).times(called).returns names
+      good_called=good_length*called
+      pi=Pathname.any_instance
+      pi.expects(:mtime).times(good_called).returns Time.now
+      pi.expects(:file?).times(good_called).returns true
+      assert_equal good_length, dp.find(:all)    .length
+      assert_equal bad_length,  dp.find_bad_names.length
+    end
+  end
+
   test "names embedded with a single bad character..." do
     is_thumbnail=false
     time=Time.now
-#    web_picture_extensions = %w[ .gif .giff .jpeg .jpg .png ]
-    n=(0..3).to_a
-    two=Array.new 2
+    boolean=[false,true]
+    n=(0..4).to_a
 # Use directory separator (?/), null (?\0) & pad (?\377) as bad embedded
 # characters:
     good_c = [?A..?Z, ?a..?z, ?0..?9, [?-, ?.] ].map{|e| e.to_a}.flatten
     all_byte_characters = (0...2**8).to_a
     bad_c = all_byte_characters - good_c
     good,bad=[good_c,bad_c].map{|a| a.map do |c|
-      before,after=two.map{Array.new(n.choice).map{good_c.choice}}
-#      (before + [c] + after).flatten.map{|e| e.chr}.to_s +
-#          web_picture_extensions.choice
-      (before + [c] + after).flatten.map{|e| e.chr}.to_s
+      name=Array.new(n.choice).map{good_c.choice}
+      name.insert((0..name.length).to_a.choice,c).map{|e| e.chr}.to_s
     end }
-    [good, bad].each{|a| assert a.length > 0}
-    dp=DirectoryPicture
-    pi=Pathname.any_instance
+    [good, bad].each{|a| assert_present a}
 # When all good or all bad...:
 # Finding good & bad names should find all or none:
     [   [good,  good.length, 0         ],
         [bad,   0,           bad.length]   ].each do
         |names, good_length, bad_length|
       called=2
+      dp=DirectoryPicture
       dp.expects(:gallery_directory        ).times(called).returns @tests
       dp.expects(:gallery_directory_entries).times(called).returns names
       good_called=good_length*called
+      pi=Pathname.any_instance
       pi.expects(:mtime).times(good_called).returns Time.now
       pi.expects(:file?).times(good_called).returns true
       assert_equal good_length, dp.find(:all)    .length
@@ -54,6 +81,9 @@ class DirectoryPictureTest < ActiveSupport::TestCase
 
 #-------------
   private
+
+  def check_good_and_bad_names
+  end
 
   def make_reproducible_tests_involving_array_choice
     Kernel.srand 0
